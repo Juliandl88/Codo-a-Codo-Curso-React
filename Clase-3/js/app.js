ad4142795627d9daf878b5e9012db2b4144ad306
Julian <julian_delisio@hotmail.com>
2022-09-05 00:44:31 -0300

    Clase 3

--- Clase-3/js/app.js
+++ Clase-3/js/app.js
@@ -1,13 +1,37 @@
 const myDoc = document;
 
-console.log(document);
-
-//let myH1 = myDoc.getElementByTagName('h1');
+let myH1 = myDoc.getElementsByTagName('h1');
 
 let myH3 = myDoc.getElementById("h3");
 
-console.log(myH3);
-console.log(myH1);
+let contenedorVideo = document.getElementById("video");
+
+//myH3.innerHTML = "Estoy modificando el texto dentro del h3";
+//const myButtonStr = "<br><button>Esto es un button</button><br>"
 
 
-// 
+const linkVideo = '<iframe width="560" height="315" src="https://www.youtube.com/embed/LU99kUnWW3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+/*for (let i = 0; i < cant_element; i++){
+    contenedorVideo += linkVideo;
+}
+*/
+
+let myVideos = myDoc.getElementById("valorInput");
+
+const handleInput = () =>{
+    let inputValue = document.getElementById("getVideo").value; // Obtengo el valor del input
+    
+    if(isNaN(inputValue) || inputValue>10){
+        alert("Por favor ingrese un número del 1 al 10")
+    }else{
+        myDoc.getElementById("valorInput").innerHTML = "Cantidad de videos generados: " + inputValue;
+        let num = Number(inputValue)
+        myDoc.getElementById("numVideos").innerHTML    
+            for (let index = 0; index < num; index++) {
+                myVideos.innerHTML = myVideos.innerHTML + linkVideo;
+            }
+            
+    }
+  }
+
---
+++ Clase-3/readme.md
@@ -0,0 +1,11 @@
+Actividades.
+
+Partir del ejercicio 2 y van a agregar lo que vieron de la siguiente manera:
+Agrega etiqueta video por codigo(sacar url desde youtube)
+La etiqute la van a repetir N veces en funcion de una constante/variable definida.
+Todo debe estar dentro de una lista
+
+opcional
+
+realizar esto sumando class de Bootstrap.
+Pueden usar un input para cambiar la cantidad de veces que se repite el video.
